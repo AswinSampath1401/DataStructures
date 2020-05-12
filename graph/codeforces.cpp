187409f23f55b170dd4cdd24c0361dadb2a8ddec
Aswin Sampath <aswinsampath1401@gmail.com>
2020-05-12 17:46:22 +0530

    Dijik

---
+++ graph/codeforces.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+
+ 
+using namespace std;
+typedef long long ll;
+const int N=1e5;
+const int W=1e6;
+const int INF=1e9;
+vector<pair<int, int>> g[N];
+int dist[N];
+bool visited[N];
+    
+int main()
+{
+        ll n, m;
+        cin>>n>>m;
+        assert(2<=n && n<=N);
+        for(int i = 0; i < n; ++i) g[i].clear();
+        for(int i = 0; i < m; i++)
+        {
+            ll v, u, w;
+            cin>>v>>u>>w;
+            assert(1 <= v && v <= n);
+            assert(1 <= u && u <= n);
+            assert(1 <= w && w <= W);
+            --v; --u;
+            g[v].push_back({u, w});
+            g[u].push_back({v, w});
+        }
+        int s;
+        scanf("%d", &s);
+        --s;
+        
+        fill(dist, dist + n, INF);
+        fill(visited, visited + n, 0);
+        dist[s] = 0;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+        q.push({0, s});
+        while(!q.empty())
+        {
+            int v = q.top().second;
+            int d = q.top().first;
+            q.pop();
+            if(visited[v]) continue;
+            visited[v] = 1;
+            for(auto it: g[v])
+            {
+                int u = it.first;
+                int w = it.second;
+                if(dist[v] + w < dist[u])
+                {
+                    dist[u] = dist[v] + w;
+                    q.push({dist[u], u});
+                }
+            }
+        }
+        for(int i = 0; i < n; ++i)
+        {
+            if(i == s) continue;
+            printf("%d ", (dist[i] != INF ? dist[i] : -1));
+        }
+        printf("\n");
+    return 0;
+}
---
+++ graph/dijikhack.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+
+
+#define space " "
+#define endl '\n'
+#define pb(x) push_back(x)
+#define mp(a,b) make_pair(a,b)
+#define FAST {ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);}
+#define testcase ll t; cin>>t; while(t--)
+
+
+
+const int INF=1e9;
+const int MOD=1e9+7;
+
+void solve(){
+    printf("Hello\n");
+}
+
+
+int main(int argc, char const *argv[])
+{
+    FAST;
+    testcase{
+        solve();
+    }
+    return 0;
+}
+
